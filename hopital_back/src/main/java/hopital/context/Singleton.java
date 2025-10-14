5be1099ab0e964ffbb7c760496682927043f259a
NailimN <milian.normantlegall@gmail.com>
2025-10-14 16:45:54 +0200

    DaoCompte

--- hopital_back/src/main/java/hopital/context/Singleton.java
+++ hopital_back/src/main/java/hopital/context/Singleton.java
@@ -3,9 +3,9 @@ package hopital.context;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import hopital.dao.DAOCompteJDBC;
-import hopital.dao.DAOPatientJDBC;
-import hopital.dao.DAOVisiteJDBC;
+import hopital.dao.DAOCompte;
+import hopital.dao.DAOPatient;
+import hopital.dao.DAOVisite;
 import hopital.dao.IDAOCompte;
 import hopital.dao.IDAOPatient;
 import hopital.dao.IDAOVisite;
