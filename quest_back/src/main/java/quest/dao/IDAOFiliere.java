23906f3ef17d24d50544ee13202e31c2db272360
Bayshunpu <nolwen.berton@outlook.fr>
2025-10-14 17:28:28 +0200

    Erratum IDAOFiliere
    Retirer fonction withventes prb

--- quest_back/src/main/java/quest/dao/IDAOFiliere.java
+++ quest_back/src/main/java/quest/dao/IDAOFiliere.java
@@ -7,6 +7,4 @@ import quest.model.Filiere;
 public interface IDAOFiliere extends IDAO<Filiere, Integer> {
 	
 	public List<Filiere> findByLibLike(String libelle);
-	public Filiere findByIdWithVentes(Integer idFiliere);
-
 }
