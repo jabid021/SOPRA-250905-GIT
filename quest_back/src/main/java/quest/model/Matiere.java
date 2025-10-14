a9e7a6a008bb9e6b35d20c02dc6b3640ab710681
costejb <jeanbaptistecoste@orange.fr>
2025-10-14 17:14:59 +0200

    Matiere
    DAO IDAO et mapping

--- quest_back/src/main/java/quest/model/Matiere.java
+++ quest_back/src/main/java/quest/model/Matiere.java
@@ -1,8 +1,23 @@
 package quest.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Matiere {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	
+	@Column(name="libelle",nullable = false,columnDefinition = "VARCHAR(50)")
 	private String libelle;
+	
+	public Matiere() {}
 	
 	
 	public Matiere(Integer id, String libelle) {
