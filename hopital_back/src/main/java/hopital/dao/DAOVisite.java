3bb77b6288962f369967399a600234e2eb01dc71
paulpts <p.pontois@outlook.com>
2025-10-14 17:17:25 +0200

    Tout le TP hopital

--- hopital_back/src/main/java/hopital/dao/DAOVisite.java
+++ hopital_back/src/main/java/hopital/dao/DAOVisite.java
@@ -2,44 +2,67 @@ package hopital.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import hopital.context.Singleton;
+import hopital.model.Patient;
 import hopital.model.Visite;
 
 public class DAOVisite implements IDAOVisite {
 
 	@Override
 	public List<Visite> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		List<Visite> visites  = em.createQuery("from Visite").getResultList();
+		em.close();
+		return visites;
 	}
 
 	@Override
 	public Visite findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		Visite visite  = em.find(Visite.class, id);
+		em.close();
+		return visite;
 	}
 
 	@Override
 	public Visite save(Visite visite) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		visite=  em.merge(visite);
+		em.getTransaction().commit();
+		em.close();
+		return visite;
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		Visite visite = em.find(Visite.class, id);
+		em.remove(visite);
+		em.getTransaction().commit();
+		em.close();
+	
 	}
 
 	@Override
 	public void delete(Visite visite) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		visite = em.merge(visite);
+		em.remove(visite);
+		em.getTransaction().commit();
+		em.close();
 	}
 
 	@Override
 	public List<Visite> findAllByIdPatient(Integer idPatient) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		List<Visite> visites  = em.createQuery("SELECT v from Visite v where v.patient.id=:id").setParameter("id", idPatient).getResultList();
+		em.close();
+		return visites;
 	}
 
 }
