7aecc49876d6b2289b2fc14f0171b9d9e2c78862
PaulineFss <pauline.fassy@outlook.fr>
2025-10-15 10:16:44 +0200

    ajout IDAO et DAO Stagiaire et Employe

--- quest_back/src/main/java/quest/model/Stagiaire.java
+++ quest_back/src/main/java/quest/model/Stagiaire.java
@@ -18,6 +18,10 @@ public class Stagiaire extends Personne{
 	@ManyToOne
 	private Filiere filiere;
 	
+	
+	public Stagiaire() {
+	}
+
 	public Stagiaire(Integer id, String login, String password, String nom, String prenom,Civilite civilite, String email,
 			String numero,String voie,String ville,String cp, Ordinateur ordinateur,Filiere filiere) {
 		super(id, login, password, nom, prenom,civilite);
