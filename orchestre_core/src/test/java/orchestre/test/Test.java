dc074c9018364c42b697fa66c7673e1be24364d5
MaxRK <max.rekar@gmail.com>
2025-10-28 10:03:28 +0100

    Auto stash before merge of "main" and "origin/main"

--- orchestre_core/src/test/java/orchestre/test/Test.java
+++ orchestre_core/src/test/java/orchestre/test/Test.java
@@ -2,21 +2,18 @@ package orchestre.test;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import orchestre.model.Flutiste;
-import orchestre.model.Guitariste;
 import orchestre.model.IMusicien;
-import orchestre.model.Pianiste;
 
 public class Test {
 
 	public static void main(String[] args) {
 	
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:application-context.xml");
+		 ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:application-context.xml");
 
-		Guitariste guitariste=(Guitariste) ctx.getBean("guitariste");
-		IMusicien pianiste=(Pianiste) ctx.getBean("pianiste");;
-		IMusicien flutiste=(Flutiste) ctx.getBean("flutiste");;
 		
+		IMusicien guitariste= (IMusicien) ctx.getBean("guitariste");
+		IMusicien pianiste=(IMusicien) ctx.getBean("pianiste");
+		IMusicien flutiste=(IMusicien) ctx.getBean("flutiste");
 		
 		guitariste.jouer();
 		pianiste.jouer();
