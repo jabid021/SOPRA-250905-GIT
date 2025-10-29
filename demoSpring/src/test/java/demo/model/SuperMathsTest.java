2b054434f312d9698c2d53e7c75a093364b7d86a
MaxRK <max.rekar@gmail.com>
2025-10-29 17:05:43 +0100

    Auto stash before merge of "main" and "origin/main"

--- demoSpring/src/test/java/demo/model/SuperMathsTest.java
+++ demoSpring/src/test/java/demo/model/SuperMathsTest.java
@@ -1,5 +1,12 @@
 package demo.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -17,8 +24,17 @@ public class SuperMathsTest {
 	
 	
 	@Test
+	public void creationSuperMath() 
+	@Test
 	public void creationSuperMaths() 
 	{
+		//Arrange
+		SuperMaths sm1;
+		SuperMaths sm2;
+		//ACt
+		sm1 = new SuperMaths();
+		//Assert
+		assertNotNull(sm1);
 		SuperMaths s;
 		
 		s=new SuperMaths();
@@ -28,6 +44,13 @@ public class SuperMathsTest {
 	
 	@Test
 	public void addition() 
+	{	
+		//Arrange
+		SuperMaths sm1 = new SuperMaths();
+		//Act
+		int result =sm1.additionner(1,5);
+		//Assert
+		assertEquals(6, result);
 	{
 		int a=1;
 		int b=5;
@@ -42,6 +65,12 @@ public class SuperMathsTest {
 	@Test
 	public void soustraction() 
 	{
+		//Arrange
+		SuperMaths sm1 = new SuperMaths();
+		//Act
+		int result =sm1.soustraire(6,5);
+		//Assert
+		assertNotEquals(0, result);
 		int a=6;
 		int b=5;
 		SuperMaths s=new SuperMaths();
