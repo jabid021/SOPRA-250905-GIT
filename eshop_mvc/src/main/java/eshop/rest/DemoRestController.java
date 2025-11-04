f0324373017da3e402feb97089d7b020dc899e21
MaxRK <max.rekar@gmail.com>
2025-11-04 12:27:47 +0100

    Exo1

--- eshop_mvc/src/main/java/eshop/rest/DemoRestController.java
+++ eshop_mvc/src/main/java/eshop/rest/DemoRestController.java
@@ -1,18 +1,31 @@
 package eshop.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import eshop.model.Fournisseur;
 import eshop.model.Produit;
 
+@RestController
+@RequestMapping("/api/demo")
 public class DemoRestController {
 
-	
-	public String demo(Integer nb,String login) 
+	@GetMapping("/{nb}")
+	public String demo(@PathVariable Integer nb, String login) 
 	{
-		return "";
+		return "Requête avec les valeurs "+nb+" et "+login;
 	}
 	
-	
+	@GetMapping("/produit")
+	@ResponseBody
 	public Produit produit() 
 	{
-		return null;
+		Produit produit = new Produit();
+		Fournisseur fournisseur = new Fournisseur();
+		produit.setFournisseur(fournisseur);
+		return produit;
 	}
 }
