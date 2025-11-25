b75f55e1b6055ee32ec1a204a0e0fd817879e207
Bayshunpu <nolwen.berton@outlook.fr>
2025-11-25 15:14:06 +0100

    logger livre
    ouiiiiiiiiii

--- bibliotek/bibliotek-boot/src/main/java/fr/bibliotek/service/LivreService.java
+++ bibliotek/bibliotek-boot/src/main/java/fr/bibliotek/service/LivreService.java
@@ -2,6 +2,8 @@ package fr.bibliotek.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import fr.bibliotek.api.request.CreateOrUpdateLivreRequest;
@@ -11,6 +13,7 @@ import fr.bibliotek.repo.AuteurRepository;
 import fr.bibliotek.repo.CollectionRepository;
 import fr.bibliotek.repo.EditeurRepository;
 import fr.bibliotek.repo.LivreRepository;
+import fr.bibliotek.security.config.SecurityConfig;
 
 @Service
 public class LivreService {
@@ -18,8 +21,11 @@ public class LivreService {
     private final AuteurRepository auteurRepository;
     private final EditeurRepository editeurRepository;
     private final CollectionRepository collectionRepository;
+    private final static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
 
     public LivreService(LivreRepository repository, AuteurRepository auteurRepository, EditeurRepository editeurRepository, CollectionRepository collectionRepository) {
+        log.info("Création de AuteurService");
         this.repository = repository;
         this.auteurRepository = auteurRepository;
         this.editeurRepository = editeurRepository;
@@ -27,28 +33,34 @@ public class LivreService {
     }
 
     public List<Livre> findAll() {
+        log.info("Création de la liste des livres");
         return this.repository.findAll();
     }
 
     public Livre findById(String id) {
+        log.info("Récupération livre d'id {}", id);
         return this.repository.findById(id).orElseThrow(LivreNotFoundException::new);
     }
 
     public Livre save(CreateOrUpdateLivreRequest request) {
+        log.info("Save (ajout) du livre {} {} {}", request.getAuteurId(), request.getEditeurId(), request.getCollectionId());
         return this.save(new Livre(), request);
     }
 
     public Livre save(String id, CreateOrUpdateLivreRequest request) {
+        log.info("Save (ajout) du livre {} {} {}", request.getAuteurId(), request.getEditeurId(), request.getCollectionId());
         Livre livre = this.findById(id);
 
         return this.save(livre, request);
     }
 
     public void deleteById(String id) {
+        log.info("Création de AuteurService");
         this.repository.deleteById(id);
     }
 
     private Livre save(Livre livre, CreateOrUpdateLivreRequest request) {
+        log.info("Création de AuteurService");
         livre.setNom(request.getNom());
         livre.setResume(request.getResume());
         livre.setPublication(request.getPublication());
