e3426f01ac17dc431ab68cdf0646febd6db8152f
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-25 13:42:52 +0000

    JOUR 2 - JENKINS BOOT SONAR

--- quest-boot/src/main/java/quest/config/JwtUtils.java
+++ quest-boot/src/main/java/quest/config/JwtUtils.java
@@ -1,14 +1,14 @@
 package quest.config;
 
-import java.util.Date;
 import java.util.Optional;
 
-import javax.crypto.SecretKey;
-
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.keys.HmacKey;
+import org.jose4j.lang.JoseException;
 import org.springframework.security.core.Authentication;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 
 public class JwtUtils {
     private final static String JWT_KEY = "6E5A7234753778214125442A472D4B6150645367556B58703273357638792F42";
@@ -17,33 +17,35 @@ public class JwtUtils {
     private JwtUtils() { }
 
     public static String generate(Authentication auth) {
-        Date now = new Date();
-        SecretKey secretKey = Keys.hmacShaKeyFor(JWT_KEY.getBytes());
+        JwtClaims claims = new JwtClaims();
+        JsonWebSignature jws = new JsonWebSignature();
 
-        // Si la connexion est OK, on génère un jeton JWT
-        return Jwts.builder()
-            .subject(auth.getName()) // Souvent, c'est le username ici
-            .issuedAt(now) // Date de fabrication
-            .expiration(new Date(now.getTime() + JWT_EXPIRATION)) // Durée de validité
-            .signWith(secretKey)
-            .compact() // Le jeton JWT sous forme de String
-        ;
+        claims.setSubject(auth.getName());
+        claims.setIssuedAtToNow();
+        claims.setExpirationTimeMinutesInTheFuture((int)(JWT_EXPIRATION / 60000));
+
+        jws.setPayload(claims.toJson());
+        jws.setAlgorithmHeaderValue("HS256");
+        jws.setKey(new HmacKey(JWT_KEY.getBytes()));
+
+        try {
+            return jws.getCompactSerialization();
+        }
+
+        catch (JoseException e) {
+            return "";
+        }
     }
 
     public static Optional<String> validateAndGetSubjet(String token) {
-        SecretKey secretKey = Keys.hmacShaKeyFor(JWT_KEY.getBytes());
-
         try {
-            // Si la connexion est OK, on génère un jeton JWT
-            String username = Jwts.parser()
-                .verifyWith(secretKey) // On donne la clé pour valider le jeton
-                .build()
-                .parseSignedClaims(token) // On donne le jeton à valider
-                .getPayload() // Le contenu du jeton
-                .getSubject() // Le nom d'utilisateur
-            ;
+            JwtConsumer consumer = new JwtConsumerBuilder()
+                .setVerificationKey(new HmacKey(JWT_KEY.getBytes()))
+                .setRequireExpirationTime()
+                .build();
 
-            return Optional.ofNullable(username);
+            JwtClaims claims = consumer.processToClaims(token);
+            return Optional.ofNullable(claims.getSubject());
         }
 
         catch (Exception ex) {
