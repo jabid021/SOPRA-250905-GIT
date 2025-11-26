6df37ef26b46420a13d86dd4cc3eaa03839847df
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-26 11:14:27 +0000

    JOUR 1 - WEB BEAN PARAM

--- QUARKUS/formation-quarkus/src/main/java/fr/formation/api/ProduitResource.java
+++ QUARKUS/formation-quarkus/src/main/java/fr/formation/api/ProduitResource.java
@@ -1,9 +1,12 @@
 package fr.formation.api;
 
+import fr.formation.dto.request.CreateProduitRequest;
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
 
 @Path("/produit")
 public class ProduitResource {
@@ -17,5 +20,16 @@ public class ProduitResource {
     @GET
     public String findByIdPath(@PathParam("id") int id) {
         return "Produit " + id + " !";
+    }
+
+    @Path("/create")
+    @GET
+    public Response create(@BeanParam CreateProduitRequest request) {
+        System.out.println(request.getLibelle());
+        System.out.println(request.getPrix());
+
+        // return Response.ok().build();
+
+        return Response.ok("OK !").build();
     }
 }
