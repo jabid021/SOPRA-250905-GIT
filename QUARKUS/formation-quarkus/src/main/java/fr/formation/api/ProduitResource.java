5606985b1fee7a29546c4d6615dc7d8b90e35370
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-26 10:58:58 +0000

    JOUR 1 - WEB PATH PARAM

--- QUARKUS/formation-quarkus/src/main/java/fr/formation/api/ProduitResource.java
+++ QUARKUS/formation-quarkus/src/main/java/fr/formation/api/ProduitResource.java
@@ -2,12 +2,20 @@ package fr.formation.api;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 
 @Path("/produit")
 public class ProduitResource {
     @GET
-    public String findById(@QueryParam("id") int id) {
+    public String findByIdQuery(@QueryParam("id") int id) {
+        return "Produit " + id + " !";
+    }
+
+
+    @Path("/{id}")
+    @GET
+    public String findByIdPath(@PathParam("id") int id) {
         return "Produit " + id + " !";
     }
 }
