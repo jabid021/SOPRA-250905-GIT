2a0999c7717e7e67884e9ba9f81de0300b3e0d36
MaxRK <max.rekar@gmail.com>
2025-11-28 10:52:15 +0100

    ajout utilisateur pour securité

---
+++ g1-bibliotek/bibliotek-quarkus/src/main/java/bibliotek/model/Utilisateur.java
@@ -0,0 +1,69 @@
+package bibliotek.model;
+
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "utilisateur")
+@UserDefinition
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Username
+    @Column(name = "login")
+    private String username;
+
+    @Password 
+    private String password;
+    
+    private boolean admin;
+
+    public Utilisateur (){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    @Roles
+    public String getRole() {
+        return (this.admin) ? "admin" : "user";
+    }
+}
